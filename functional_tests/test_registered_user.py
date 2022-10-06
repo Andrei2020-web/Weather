b531b515b9ca50ffc6258e687ad314086036fc43
Asus <Asus@Asus>
2022-10-06 21:56:40 +0300

    ФТ дополнен.

--- functional_tests/test_registered_user.py
+++ functional_tests/test_registered_user.py
@@ -82,34 +82,76 @@ class RegisteredUserTest(FunctionalTest):
         self.wait_to_be_logged_out('WeatherUser1')
 
         # Пользователь является зарегистрированным пользователем
+        self.create_pre_authenticated_sessions('WeatherUser1', '%%%%%%%%')
+        self.browser.refresh()
 
         # Пользователь открывает домашнюю страницу как
         # зарегистрированный пользователь
+        self.browser.get(self.live_server_url)
+        self.wait_to_be_logged_in('WeatherUser1')
 
         # Пользователю предлагается узнать погоду в
         # конкретном городе
+        inputbox = self.browser.find_element(by=By.ID, value='id_new_city')
+        self.assertEqual(inputbox.get_attribute('placeholder'), "Введите город")
 
         # Пользователь набирает в текстовом поле "Москва"
+        inputbox.send_keys('Москва')
 
         # Когда он нажимает кнопку "Узнать", страница обновляется,
         # и теперь страница содержит данные о погоде в Москве.
+        self.browser.find_element(by=By.ID, value='id_to_learn').click()
+
+        self.wait_for_row_in_list_table('Москва')
+        self.wait_for_row_in_list_table('Температура')
+        self.wait_for_row_in_list_table('Атмосферное давление')
+        self.wait_for_row_in_list_table('Влажность')
+        self.wait_for_row_in_list_table('Ветер')
 
         # Текстовое поле по-прежнему приглашает узнать погоду в городе.
+        inputbox = self.browser.find_element(by=By.ID, value='id_new_city')
+        inputbox.clear()
+        self.assertEqual(inputbox.get_attribute('placeholder'), "Введите город")
 
         # Пользователь набирает в текстовом поле "Сочи"
+        inputbox.send_keys('Сочи')
 
         # Когда он нажимает "Узнать", страница снова обновляется, и теперь
         # показывает данные о погоде в "Сочи" и в "Москва"
+        self.browser.find_element(by=By.ID, value='id_to_learn').click()
+        self.wait_for_row_in_list_table('Сочи')
+        self.wait_for_row_in_list_table('Москва')
 
         # Далее пользователь замечает ссылку "Удалить"
         # над данными о погоде для городов "Москва" и "Сочи"
+        self.wait_for(lambda: self.browser.find_element(by=By.ID,
+                                                        value='id_delete_Москва'))
+        self.wait_for(lambda: self.browser.find_element(by=By.ID,
+                                                        value='id_delete_Сочи'))
 
         # Пользователь нажимает на ссылку "Удалить" для города "Москва"
+        self.browser.find_element(by=By.ID, value='id_delete_Москва').click()
 
         # Страница обновляется, и теперь показывает только данные о погоде
         # в "Сочи" данные о погоде в "Москва" пропадают
+        self.wait_for_row_in_list_table('Сочи')
+        self.wait_for_row_in_list_table('Температура')
+        self.wait_for_row_in_list_table('Атмосферное давление')
+        self.wait_for_row_in_list_table('Влажность')
+        self.wait_for_row_in_list_table('Ветер')
+
+        page_text = self.wait_for(
+            lambda: self.browser.find_element(by=By.TAG_NAME,
+                                              value='body').text)
+        self.assertNotIn('Москва', page_text)
 
         # Пользователь ещё раз нажимает на ссылку "Удалить",
         # но уже для города "Сочи"
+        self.browser.find_element(by=By.ID, value='id_delete_Сочи').click()
 
         # Страница обновляется, данные о погоде в "Сочи" пропадают
+        page_text = self.wait_for(
+            lambda: self.browser.find_element(by=By.TAG_NAME,
+                                              value='body').text)
+        self.assertNotIn('Москва', page_text)
+        self.assertNotIn('Сочи', page_text)
